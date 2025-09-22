4cb44c56cf8b851b429b3ea140d092fee9c41da0
Driely Santos <santosdriely98@gmail.com>
2025-09-22 19:12:20 -0300

    feat: atualizar a estrutura do componente Header para melhorar a semântica e a organização do código

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 function Header(){
     return(
-        <header className="header">
-            <a href="#">
-                <h1 className="logo">DS</h1>
-            </a>
-            <nav className="menu">
-                <ul>
-                    <li><a href="#info-sobre">+ sobre mim</a></li>
-                    <li><a href="#formacoes">Formações</a></li>
-                    <li><a href="#projetos">Projetos</a></li>
-                    <li className="menu__sobre-mim"><a href="#info-sobre">+ sobre mim</a></li>
-                </ul>
-            </nav>
-        </header>
+            <div className="cabecalho">
+                <a href="#">
+                    <h1 className="logo">DS</h1>
+                </a>
+                <nav className="menu">
+                    <ul>
+                        <li><a href="#formacoes">Formações</a></li>
+                        <li><a href="#projetos">Projetos</a></li>
+                        <li><a href="#info-sobre">+ sobre mim</a></li>
+                        <li className="menu__sobre-mim"><a href="#info-sobre">+ sobre mim</a></li>
+                    </ul>
+                </nav>
+            </div>
     )
 }
 
