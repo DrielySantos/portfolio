27d77eddb38f15afbc876d74d2ccc741e15af760
Driely Santos <santosdriely98@gmail.com>
2025-09-23 20:04:32 -0300

    feat: atualizar estilos e conteúdo do componente About e Studies para melhor apresentação e semântica

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,12 @@ function About() {
             <div className="container__info-sobre">
                 <div className="bio">
                     <h2 className="bio__titulo">Sobre mim</h2>
-                    <p className="bio__texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tellus sit amet nisl tempor eleifend. Sed quis purus nec nunc feugiat laoreet. </p>
+                    <p>Sou uma pessoa curiosa e apaixonada por aprender coisas novas.</p>
+                    <p> A tecnologia entrou na minha vida como um desafio e acabou se tornando um grande propósito: transformar ideias em projetos reais e crescer a cada linha de código.</p>
+                    <p>
+                    Estou me dedicando ao desenvolvimento frontend, estudando com foco em JavaScript e React, mas sem medo de explorar além do que conheço. Tenho um jeito persistente de lidar com dificuldades, gosto de me testar em novos projetos, pedir feedbacks e compartilhar aprendizados, porque acredito que crescer junto é sempre mais valioso.</p>
+                    <p>
+                    Fora do código, também carrego muito da minha personalidade criativa e leve: adoro jogar video game para relaxar, maratonar um bom K-drama e sonhar alto sobre os caminhos que posso trilhar. Para mim, tecnologia não é só carreira, é também uma forma de expressão e de impactar pessoas.</p>
                 </div>
                 <Skills />
                 <Hobbies />
--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -4,41 +4,41 @@ import LogoUniCV from '../assets/image/img-unicv.png'
 
 function Studies(){
     return(
-        <div class="formacoes" id="formacoes">
-            <h2 class="formacoes__titulo">Formação Acadêmica</h2> 
-            <div class="formacoes__cursos">
-                <div class="container__formacoes-box">
-                    <ul class="formacoes__cursos__list">
-                        <li class="formacoes__cursos__item__img">
+        <section className="formacoes" id="formacoes">
+            <h2 className="formacoes__titulo">Formação Acadêmica</h2> 
+            <div className="formacoes__cursos">
+                <div className="container__formacoes-box">
+                    <ul className="formacoes__cursos__list">
+                        <li className="formacoes__cursos__item__img">
                             <img src={LogoSenac} alt="Logo da Senac"></img></li>
-                        <li class="formacoes__cursos__item__titulo"><h4>Programação Web</h4></li>
-                        <li class="formacoes__cursos__item__subtitulo"><p>2023 - SENAC</p></li>
+                        <li className="formacoes__cursos__item__titulo"><h4>Programação Web</h4></li>
+                        <li className="formacoes__cursos__item__subtitulo"><p>2023 - SENAC</p></li>
                     </ul>
                 </div>
-                <div class="container__formacoes-box">
-                    <ul class="formacoes__cursos__list">
-                        <li class="formacoes__cursos__item__img">
+                <div className="container__formacoes-box">
+                    <ul className="formacoes__cursos__list">
+                        <li className="formacoes__cursos__item__img">
                             <img src={LogoAlura} alt="Logo da Alura"></img>
                         </li>
-                        <li class="formacoes__cursos__item__titulo"><h4>Front End - T6</h4></li>
-                        <li class="formacoes__cursos__item__subtitulo">
+                        <li className="formacoes__cursos__item__titulo"><h4>Front End - T6</h4></li>
+                        <li className="formacoes__cursos__item__subtitulo">
                             <p>2024 - Oracle/ Alura Latam<span></span></p>
                         </li>
                     </ul>
                 </div>
-                <div class="container__formacoes-box">
-                    <ul class="formacoes__cursos__list">
-                        <li class="formacoes__cursos__item__img">
+                <div className="container__formacoes-box">
+                    <ul className="formacoes__cursos__list">
+                        <li className="formacoes__cursos__item__img">
                             <img src={LogoUniCV} alt="Logo da UniCV"></img>
                         </li>
-                        <li class="formacoes__cursos__item__titulo"><h4>Análise e Desenvolvimento de Sistemas</h4></li>
-                        <li class="formacoes__cursos__item__subtitulo">
+                        <li className="formacoes__cursos__item__titulo"><h4>Análise e Desenvolvimento de Sistemas</h4></li>
+                        <li className="formacoes__cursos__item__subtitulo">
                             <p>2025 - UniCV (1º período)<span></span></p>
                         </li>
                     </ul>
                 </div>
             </div>      
-        </div>
+        </section>
     )
 }
 
